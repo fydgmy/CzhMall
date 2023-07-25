d91e201663b55a9175fc8303f88c8990c004d682
chenzihao <1046683895@qq.com>
2023-07-25 23:23:59 +0800

    p66商品服务API品牌管理JSR303数据校验
    在后端使用@NotEmpty
    	@NotBlank
    getFieldErrors等获取校验的错误结果

--- gulimall/gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -11,7 +11,16 @@ mybatis-plus官网：https://baomidou.com/pages/6b03c5/
 1)配置全局的逻辑删除规则（省略）
 2）配置逻辑删除的组件Bean（省略）
 3）给Bean加上逻辑删除注解@TableLogic
- */  //aa
+ */  //
+/*
+JSR303校验
+1.给bean添加校验注解:javax.validation.constraints 并定义自己的message提示
+2.开启校验功能@Valid
+效果：校验错误后会有默认的响应
+3.给校验的bean后紧跟一个bingdingresult。就可以获取到校验结果
+
+
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.example.gulimall.product.dao")
--- gulimall/gulimall-product/src/main/java/com/example/gulimall/product/entity/BrandEntity.java
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/entity/BrandEntity.java
@@ -6,6 +6,9 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
@@ -22,31 +25,47 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌id
 	 */
+	@NotEmpty
 	@TableId
 	private Long brandId;
 	/**
 	 * 品牌名
+	 * @NotNull 不能为空null
+	 * @NotEmpty 不为null或不为空串
+	 * @NotBlank 必须包含至少一个非空字符 不能为一个空格
+	 *
 	 */
+	@NotEmpty
+	@NotBlank
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotEmpty
+	@URL(message="logo必须是一个合法的url地址")
 	private String logo;
 	/**
 	 * 介绍
 	 */
+	@NotEmpty
 	private String descript;
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+	@NotEmpty
 	private Integer showStatus;
 	/**
 	 * 检索首字母
+	 * 自定义规则没有相应注解，但可以使用自定义Pattern
 	 */
+	@NotEmpty
+	@Pattern(regexp = "/^[a-zA-Z]$/",message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotEmpty
+	@Min(value = 0)
 	private Integer sort;
 
 }
