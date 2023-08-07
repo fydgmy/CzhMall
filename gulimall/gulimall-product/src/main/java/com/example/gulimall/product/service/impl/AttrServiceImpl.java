e9b863831f32a5a0d541986ca9eb646c9c90bae8
chenzihao <1046683895@qq.com>
2023-08-07 20:28:09 +0800

    p76商品服务api平台属性规格参数列表
    平时我们在entity中加入注解，表示该属性在表中不存在，但是不规范，可以使用vo，将前端传入的数据封装成类来表示，并进行数据处理封装为数据库对应的po，上传至数据库中

--- gulimall/gulimall-product/src/main/java/com/example/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.gulimall.product.service.impl;
 
+import com.alibaba.spring.util.BeanUtils;
+import com.example.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.example.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.example.gulimall.product.vo.AttrVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,11 +16,13 @@ import com.example.common.utils.Query;
 import com.example.gulimall.product.dao.AttrDao;
 import com.example.gulimall.product.entity.AttrEntity;
 import com.example.gulimall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
-
+    @Autowired
+    AttrAttrgroupRelationDao attrAttrgroupRelationDao;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -24,6 +31,23 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        //对应数据库的实体
+        AttrEntity attrEntity = new AttrEntity();
+        //采用如下方式，一个一个属性对应会很麻烦，所以采用spring提供的工具类BeanUtils
+       // attrEntity.setAttrName(attr.getAttrName());
+        //前提条件是vo中的属性名和po的属性名相同
+        org.springframework.beans.BeanUtils.copyProperties(attr,attrEntity);
+        //保存基本数据
+        this.save(attrEntity);
+        //保存关联关系
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrAttrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId());
+        attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
+        attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
     }
 
 }
