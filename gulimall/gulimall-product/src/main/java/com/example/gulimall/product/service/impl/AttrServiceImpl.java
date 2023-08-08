00f54d35b1b090edeb76b5bab59c9aa5ff91cfb0
chenzihao <1046683895@qq.com>
2023-08-08 10:15:32 +0800

    p77商品服务api平台属性规格参数列表
    设计了vo独立于po，po针对于数据库表，而vo针对于前端传入的封装数据类，将vo类解析处理后，将数据传到数据库中

--- gulimall/gulimall-product/src/main/java/com/example/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,12 +1,24 @@
 package com.example.gulimall.product.service.impl;
 
-import com.alibaba.spring.util.BeanUtils;
+import com.aliyun.oss.common.utils.StringUtils;
 import com.example.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.example.gulimall.product.dao.AttrGroupDao;
+import com.example.gulimall.product.dao.CategoryDao;
 import com.example.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.example.gulimall.product.entity.AttrGroupEntity;
+import com.example.gulimall.product.entity.CategoryEntity;
+import com.example.gulimall.product.vo.AttrResponseVo;
 import com.example.gulimall.product.vo.AttrVo;
+import org.eclipse.jetty.util.StringUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -22,7 +34,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
     @Autowired
+    AttrGroupDao attrGroupDao;
+    @Autowired
     AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+    @Autowired
+    CategoryDao categoryDao;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -48,6 +64,45 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         attrAttrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId());
         attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
         attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
+    }
+
+    @Override
+    public PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId) {
+        QueryWrapper<AttrEntity> queryWrapper = new QueryWrapper<>();
+        if(catelogId!=0){
+            queryWrapper.eq("catelog_id",catelogId);
+        }
+        String key = (String)params.get("key");
+        if(!StringUtils.isNullOrEmpty(key)){
+            //attr_id attr_name
+            queryWrapper.and((wrapper)->{
+                wrapper.eq("attr_id",key).or().like("attr_name",key);
+            });
+        }
+        IPage<AttrEntity> page = this.page(
+                new Query<AttrEntity>().getPage(params),queryWrapper
+        );
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        //流式编程
+        List<AttrResponseVo> respVos = records.stream().map((attrEntity -> {
+            AttrResponseVo attrResponseVo = new AttrResponseVo();
+            BeanUtils.copyProperties(attrEntity, attrResponseVo);
+            //1.设置分类和分组的名字
+            AttrAttrgroupRelationEntity attrId = attrAttrgroupRelationDao.selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrEntity.getAttrId()));
+            if (attrId != null) {
+                Long attrGroupId = attrId.getAttrGroupId();
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrGroupId);
+                attrResponseVo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getCatelogId());
+            if (categoryEntity != null) {
+                attrResponseVo.setCatelogName(categoryEntity.getName());
+            }
+            return attrResponseVo;
+        })).collect(Collectors.toList());
+        pageUtils.setList(respVos);
+        return pageUtils;
     }
 
 }
---
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/vo/AttrResponseVo.java
@@ -0,0 +1,13 @@
+package com.example.gulimall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class AttrResponseVo extends AttrVo{
+    /*
+        *"catelogName": "手机/数码/手机", //所属分类名字
+		*	"groupName": "主体", //所属分组名字
+     */
+    private String catelogName;
+    private String groupName;
+}
