f2bf647407a335a86834d35339c34c4745194f05
chenzihao <1046683895@qq.com>
2023-08-09 15:49:18 +0800

    p86商品服务api新增商品商品新增vo抽取

---
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/vo/Attr.java
@@ -0,0 +1,38 @@
+/**
+  * Copyright 2023 bejson.com 
+  */
+package com.example.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2023-08-09 15:41:43
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Attr {
+
+    private int attrId;
+    private String attrName;
+    private String attrValue;
+    public void setAttrId(int attrId) {
+         this.attrId = attrId;
+     }
+     public int getAttrId() {
+         return attrId;
+     }
+
+    public void setAttrName(String attrName) {
+         this.attrName = attrName;
+     }
+     public String getAttrName() {
+         return attrName;
+     }
+
+    public void setAttrValue(String attrValue) {
+         this.attrValue = attrValue;
+     }
+     public String getAttrValue() {
+         return attrValue;
+     }
+
+}
---
+++ gulimall/gulimall-product/src/main/java/com/example/gulimall/product/vo/Bounds.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2023 bejson.com 
+  */
+package com.example.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2023-08-09 15:41:43
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Bounds {
+
+    private int buyBounds;
+    private int growBounds;
+    public void setBuyBounds(int buyBounds) {
+         this.buyBounds = buyBounds;
+     }
+     public int getBuyBounds() {
+         return buyBounds;
+     }
+
+    public void setGrowBounds(int growBounds) {
+         this.growBounds = growBounds;
+     }
+     public int getGrowBounds() {
+         return growBounds;
+     }
+
+}
